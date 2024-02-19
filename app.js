a9bf88e3d11a504fe42e42259c7fc7467a82d108
ranasingh99 <sanketverma852@gmail.com>
2024-02-19 18:16:32 +0530

    updated flow of code and got the value of msg and username

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 const express = require("express");
-const path = require("path");
+
+const bodyParser = require("body-parser");
+
 
 
 
 const app = express();
 
-
-app.use('/login',(req,res,next)=>{
-    res.send( 
-               `<form onsubmit ="localStorage.setItem('username',document.getElementById('username').value)" action="/home" method="POST">
-                    <input type = "text" id="username" name = "username" placeholder="username">
-                    <button type="submit">Login</button>
-                </form>`
-
-            )
+app.use(bodyParser.urlencoded({extended:false}));
 
 
-   // res.sendFile(path.join(__dirname+'/index.html'));
-})
-app.use('/home',(req,res,next)=>{
-   // console.log(req.body.username);
-    res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
     res.send(`
-        <form action ="/" method ="POST" onsubmit="document.getElementById(username).value"="localStorage.getItem(username)">
-        <input type = "text" name = "msg">
+        <form action ="/" method ="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')">
+        <input type = "text" name = "message">
         <input type="hidden" name="username" id="username">
         <button type ="submit">SEND</button>
        </form>
     `)
     
 })
+app.post('/',(req,res,next)=>{
+    console.log(req.body.message);
+    console.log(req.body.username);
+})
 
+app.get('/login',(req,res,next)=>{
+    res.send( 
+               `<form onsubmit ="localStorage.setItem('username',document.getElementById('username').value)" action="/login" method="POST">
+                    <input type = "text" id="username" name = "username" placeholder="username">
+                    <button type="submit">Login</button>
+                </form>`
+
+            )
+           
+
+
+   // res.sendFile(path.join(__dirname+'/index.html'));
+})
+app.post('/login',(req,res,next)=>{
+    res.redirect('/');
+})
 app.listen(3000);
